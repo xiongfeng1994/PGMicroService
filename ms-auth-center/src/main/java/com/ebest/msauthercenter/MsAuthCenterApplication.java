8428b613a0f5a5d1de39e3f8dbe2e25bc948118a
xiong <xiong@LAPTOP-BGH2DBNG>
2018-06-18 15:23:21 +0800

    ms-auth-center

---
+++ ms-auth-center/src/main/java/com/ebest/msauthercenter/MsAuthCenterApplication.java
@@ -0,0 +1,18 @@
+package com.ebest.msauthercenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring boot app.
+ *
+ * @author chenxj 2018/06/01
+ */
+@SpringBootApplication
+public class MsAuthCenterApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(MsAuthCenterApplication.class, args);
+    }
+}
---
+++ ms-auth-center/src/main/java/com/ebest/msauthercenter/SecurityConfig.java
@@ -0,0 +1,54 @@
+package com.ebest.msauthercenter;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+/**
+ * Config login authentication.
+ *
+ * @author chenxj 2018/06/01
+ */
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired JwtAuthenticationConfig config;
+
+    @Bean
+    public JwtAuthenticationConfig jwtConfig() {
+        return new JwtAuthenticationConfig();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin").password("admin").roles("ADMIN", "USER").and()
+                .withUser("chenxj").password("chenxj").roles("USER");
+    }
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .csrf().disable()
+                .logout().disable()
+                .formLogin().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                    .anonymous()
+                .and()
+                    .exceptionHandling().authenticationEntryPoint(
+                            (req, rsp, e) -> rsp.sendError(HttpServletResponse.SC_UNAUTHORIZED))
+                .and()
+                    .addFilterAfter(new JwtUsernamePasswordAuthenticationFilter(config, authenticationManager()),
+                            UsernamePasswordAuthenticationFilter.class)
+                .authorizeRequests()
+                    .antMatchers(config.getUrl()).permitAll()
+                    .anyRequest().authenticated();
+    }
+}
